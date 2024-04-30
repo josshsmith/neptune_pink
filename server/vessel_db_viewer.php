bfbce33b4f3f364de94165ac8324f2f0d6896d77
Cameron  Miller <cameronmiller@192-168-1-155.tpgi.com.au>
2024-05-01 01:07:02 +1000

    Began development of web interface

---
+++ server/vessel_db_viewer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Vessel Table</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!--<link rel="stylesheet" href="style.css">-->
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <nav>
+        <div id="logo">
+            <img src="images/anchor.png" id="anchor_icon">
+            <h2>Neptune <strong>Pink</strong></h2>
+        </div>
+        <table>
+            <tr>
+                <td><a class="page-link" href="/">Vessel Map</a></td>
+                <td><a class="page-link" href="/neptune_pink_server/vessel_db_viewer.php">View Vessel Database</a></td>
+            </tr>
+        </table>
+    </nav>
+    <div id="section">
+        <div id="side-menu">
+            Side menu
+        </div>
+        <div id="table-viewer">
+            Table 
+        </div>
+    </div>
+
+</body>
+</html>

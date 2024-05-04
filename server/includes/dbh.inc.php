197e6f6c58bf2ee0f3a87d62d9b5d4816a2b8fbc
Cameron  Miller <cameronmiller@192-168-1-155.tpgi.com.au>
2024-05-05 01:00:59 +1000

    security fix

--- server/includes/dbh.inc.php
+++ server/includes/dbh.inc.php
@@ -2,14 +2,14 @@
 
 /* Local Host
 $dsn = "mysql:host=localhost;dbname=neptune_pink"; //Data source name
-$dbusername = "root";
-$dbpassword = ""; //Change to root
+$dbusername = "###";
+$dbpassword = "###"; //Change to root
 */
 
 // inifinity
 $dsn = "mysql:host=sql109.infinityfree.com;dbname=if0_36483890_neptune_pink_db"; //Data source name
-$dbusername = "if0_36483890";
-$dbpassword = "BWzcJBMLBHf7rF"; 
+$dbusername = "###"; //Not for public repo
+$dbpassword = "###"; 
 
 
 try {

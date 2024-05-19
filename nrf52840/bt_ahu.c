0c6e80c56d98b5583bd6e9fb480e8fd8f0494d48
josshsmith <141536363+josshsmith@users.noreply.github.com>
2024-05-19 13:27:39 +1000

    Add files via upload

--- nrf52840/bt_ahu.c
+++ nrf52840/bt_ahu.c
@@ -1,10 +1,3 @@
-/*
- * ble_sensor_send.c File
- * Connor Davis - 46990275
- *
- * A file for to send sensor values from Thingy52
- */
-
 #include <bt_ahu.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -18,161 +11,146 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/types.h>
 
-#ifndef IBEACON_RSSI
-#define IBEACON_RSSI 0xc8
-#endif
-/*
- * RSSI:  -56 dBm
- */
+#define ALL_MODULES 4
 
-#define SEND_PERIOD 500
-#define MAX_NUMBER_OF_VESSELS 4
-#define MAX_DATA 25
+#define BT_CONNECTION_TYPE                                           \
+  BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_NAME, BT_GAP_ADV_FAST_INT_MIN_2, \
+                  BT_GAP_ADV_FAST_INT_MAX_2, NULL)
 
-/* New Protocol data positions */
-extern struct vessel_data* vessel_data_array;
+LOG_MODULE_DECLARE(AHU, ALL_MODULES);
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-/* iBeacon Protocol data packet */
+/* BLE data packets */
 static uint8_t vessel_data_packet1[MAX_DATA] = {
-    0x01, 0x00,                                /* Apple */
-    0x02, 0x15,                                /* iBeacon */
-    0xEF, 0x68, 0x02,        0xFF,             /* UUID[15..12] */
-    0x9B, 0x35,                                /* UUID[11..10] */
-    0x49, 0x33,                                /* UUID[9..8] */
-    0x9B, 0x10,                                /* UUID[7..6] */
-    0x52, 0xFF, 0xA9,        0x74, 0x00, 0x42, /* UUID[5..0] */
-    0xFF, 0xFF,                                /* Major */
-    0x00, 0x00, IBEACON_RSSI};
+    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 static uint8_t vessel_data_packet2[MAX_DATA] = {
-    0x02, 0x00,                                /* Apple */
-    0x02, 0x15,                                /* iBeacon */
-    0xEF, 0x68, 0x02,        0xFF,             /* UUID[15..12] */
-    0x9B, 0x35,                                /* UUID[11..10] */
-    0x49, 0x33,                                /* UUID[9..8] */
-    0x9B, 0x10,                                /* UUID[7..6] */
-    0x52, 0xFF, 0xA9,        0x74, 0x00, 0x42, /* UUID[5..0] */
-    0xFF, 0xFF,                                /* Major */
-    0x00, 0x00, IBEACON_RSSI};
+    0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 static uint8_t vessel_data_packet3[MAX_DATA] = {
-    0x03, 0x00,                                /* Apple */
-    0x02, 0x15,                                /* iBeacon */
-    0xEF, 0x68, 0x02,        0xFF,             /* UUID[15..12] */
-    0x9B, 0x35,                                /* UUID[11..10] */
-    0x49, 0x33,                                /* UUID[9..8] */
-    0x9B, 0x10,                                /* UUID[7..6] */
-    0x52, 0xFF, 0xA9,        0x74, 0x00, 0x42, /* UUID[5..0] */
-    0xFF, 0xFF,                                /* Major */
-    0x00, 0x00, IBEACON_RSSI};
+    0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 static uint8_t vessel_data_packet4[MAX_DATA] = {
-    0x04, 0x00,                                /* Apple */
-    0x02, 0x15,                                /* iBeacon */
-    0xEF, 0x68, 0x02,        0xFF,             /* UUID[15..12] */
-    0x9B, 0x35,                                /* UUID[11..10] */
-    0x49, 0x33,                                /* UUID[9..8] */
-    0x9B, 0x10,                                /* UUID[7..6] */
-    0x52, 0xFF, 0xA9,        0x74, 0x00, 0x42, /* UUID[5..0] */
-    0xFF, 0xFF,                                /* Major */
-    0x00, 0x00, IBEACON_RSSI};
-
+    0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+// array to hold data packets
 static uint8_t* vessel_data_packets[MAX_NUMBER_OF_VESSELS];
-
-static struct bt_data* vessel_data_sends[4];
 
 /* The generic bluetooth data to send */
 struct bt_data vessel_data_send1[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet1,
-            25), /* Calibrated RSSI @ 1m */
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet1, MAX_DATA),
 };
-/* The generic bluetooth data to send */
+
 struct bt_data vessel_data_send2[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet2,
-            25), /* Calibrated RSSI @ 1m */
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet2, MAX_DATA),
 };
-/* The generic bluetooth data to send */
+
 struct bt_data vessel_data_send3[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet3,
-            25), /* Calibrated RSSI @ 1m */
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet3, MAX_DATA),
 };
-/* The generic bluetooth data to send */
+
 struct bt_data vessel_data_send4[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet4,
-            25), /* Calibrated RSSI @ 1m */
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, vessel_data_packet4, MAX_DATA),
 };
 
-void print_array(uint8_t *arr, size_t size) {
-    printk("Array: ");
-    for (size_t i = 0; i < size; i++) {
-        printk("0x%02X ", arr[i]);
-    }
-    printk("$");
-}
-
+/**
+ * void speed_heading(struct vessel_data rec_info)
+ * -----------------------------------------------
+ * Update appropriate data packets with speed and heading information
+ *
+ * input: struct vessel_data rec_info - struct containing all relevant
+ * information about a single watercraft
+ */
 void speed_heading(struct vessel_data rec_info) {
-
-  int index = atoi(rec_info.position);
+  int index = atoi(rec_info.position) - 1;
   double speed = atof(rec_info.speed);
 
+  // filling first two bytes with the whole values of the float
   vessel_data_packets[index][6] = (long)(speed / 100) % 100;
   vessel_data_packets[index][7] = (long)(speed) % 100;
+  // last byte with decimal values of the float
   vessel_data_packets[index][8] = (long)(speed * 100) % 100;
 
-
   double heading = atof(rec_info.heading);
+  // filling first two bytes with the whole values of the float
   vessel_data_packets[index][9] = (long)(heading / 100) % 100;
   vessel_data_packets[index][10] = (long)(heading) % 100;
+  // last byte with decimal values of the float
   vessel_data_packets[index][11] = (long)(heading * 100) % 100;
-
 }
 
+/**
+ * void lat_long(struct vessel_data rec_info)
+ * -----------------------------------------------
+ * Update appropriate data packets with latitude and longitude information
+ *
+ * input: struct vessel_data rec_info - struct containing all relevant
+ * information about a single watercraft
+ */
 void lat_long(struct vessel_data rec_info) {
-  int index = atoi(rec_info.position);
+  int index = atoi(rec_info.position) - 1;
   double lat = atof(rec_info.latitude);
 
-  if (lat < 0) {
+  if (lat < 0) {  // if negative we set MSB
     lat = -lat;
     vessel_data_packets[index][12] = 0x80;
   } else {
     vessel_data_packets[index][12] = 0x00;
   }
+  // filling two bytes with whole number
   vessel_data_packets[index][12] |= (long)(lat / 100) % 100;
   vessel_data_packets[index][13] = (long)(lat) % 100;
+  // three bytes for decimal values
   vessel_data_packets[index][14] = (long)(lat * 100) % 100;
   vessel_data_packets[index][15] = (long)(lat * 10000) % 100;
   vessel_data_packets[index][16] = (long)(lat * 1000000) % 100;
 
   double longitude = atof(rec_info.longitude);
 
-  if (longitude < 0) {
+  if (longitude < 0) {  // if negative we set MSB
     longitude = -longitude;
     vessel_data_packets[index][17] = 0x80;
   } else {
     vessel_data_packets[index][17] = 0x00;
   }
+  // filling two bytes with whole number
   vessel_data_packets[index][17] |= (long)(longitude / 100) % 100;
   vessel_data_packets[index][18] = (long)(longitude) % 100;
+  // three bytes for decimal values
   vessel_data_packets[index][19] = (long)(longitude * 100) % 100;
   vessel_data_packets[index][20] = (long)(longitude * 10000) % 100;
   vessel_data_packets[index][21] = (long)(longitude * 1000000) % 100;
-
-
 }
 
+/**
+ * void mmsi_alter(struct vessel_data rec_info)
+ * -----------------------------------------------
+ * Update appropriate data packets with mmsi
+ *
+ * input: struct vessel_data rec_info - struct containing all relevant
+ * information about a single watercraft
+ */
 void mmsi_alter(struct vessel_data rec_info) {
   char* endptr;
-  int index = atoi(rec_info.position);
+  int index = atoi(rec_info.position) - 1;
   long long mmsi = strtol(rec_info.mmsi, &endptr, 10);
+  // putting two digits in each byte
   vessel_data_packets[index][5] = mmsi % 100;
   vessel_data_packets[index][4] = (mmsi / 100) % 100;
   vessel_data_packets[index][3] = (mmsi / 10000) % 100;
@@ -180,20 +158,24 @@ void mmsi_alter(struct vessel_data rec_info) {
   vessel_data_packets[index][1] = (mmsi / 100000000) % 100;
 }
 
+/**
+ * void alter_packet(struct vessel_data rec_info)
+ * -----------------------------------------------
+ * Update appropriate data packets
+ *
+ * input: struct vessel_data rec_info - struct containing all relevant
+ * information about a single watercraft
+ */
 void alter_packet(struct vessel_data rec_info) {
   int8_t index = (uint8_t)atoi(rec_info.position);
-  if (index < 0 && index > 4) {
+  if (index < 1 && index > 4) {
+    LOG_ERR("INDEX ERROR : %d", index);
     return;
   }
   mmsi_alter(rec_info);
   speed_heading(rec_info);
   lat_long(rec_info);
-  print_array(vessel_data_packets[index], 25);
-  
-    
 }
-
-
 
 /*
  * int bluetooth_thread(void)
@@ -203,70 +185,70 @@ void alter_packet(struct vessel_data rec_info) {
  * 1. Receive sensor data from the sensor queue,
  * 2. Update the data packet to be sent
  * 3. Update the data that is sent via bluetooth to the new data packet
- *
- * params:
- * returns:
  */
 void bt_task(void* p1, void* p2, void* p3) {
   /* Initialise bluetooth requirements */
-  int err = bt_enable(NULL);
-  // if (err) {
-  //   // printk("Bluetooth init failed (err %d)\n", err);
-  //   return 0;
-  // }
+  int err;
+  err = bt_enable(NULL);
+  if (err) {
+    LOG_ERR("ERROR IN BT ENABLE: %d", err);
+    return;
+  }
 
   /* Allow all items to initialise*/
   k_msleep(SEND_PERIOD);
 
-  /* Initialise and toggle the LED */
-  // if (!gpio_is_ready_dt(&led)) {
-  //   return 0;
-  // }
-
-  gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (err) {
+    LOG_ERR("ERROR IN LED ENABLE: %d", err);
+    return;
+  }
   gpio_pin_toggle_dt(&led);
 
+  // fill array with data packets
   vessel_data_packets[0] = vessel_data_packet1;
   vessel_data_packets[1] = vessel_data_packet2;
   vessel_data_packets[2] = vessel_data_packet3;
   vessel_data_packets[3] = vessel_data_packet4;
 
-
-  vessel_data_sends[0] = vessel_data_send1;
-  vessel_data_sends[1] = vessel_data_send2;
-  vessel_data_sends[2] = vessel_data_send3;
-  vessel_data_sends[3] = vessel_data_send4;
-
   /* Start a generic, connectible, scannable bluetooth advertising */
   /* Create a non-connectable non-scannable advertising set */
-  err = bt_le_adv_start(BT_LE_ADV_NCONN_NAME, vessel_data_send1,
+  err = bt_le_adv_start(BT_CONNECTION_TYPE, vessel_data_send1,
                         ARRAY_SIZE(vessel_data_send1), NULL, 0);
-
-  printk("Bluetooth Successfully Initialised\n");
+  if (err) {
+    LOG_ERR("ERROR IN BT ADVERTISING ENABLE: %d", err);
+    return;
+  }
 
   /* Enter the infinite loop after initialising*/
- 
+  LOG_INF("Beginning bt loop");
   while (true) {
     for (int i = 0; i < 4; i++) {
-      switch(i) {
+      switch (i) {
+        // alternating which packet is updated
         case 0:
-          bt_le_adv_update_data(vessel_data_send1, ARRAY_SIZE(vessel_data_send1), NULL, 0);
+          err = bt_le_adv_update_data(vessel_data_send1,
+                                      ARRAY_SIZE(vessel_data_send1), NULL, 0);
           break;
         case 1:
-          bt_le_adv_update_data(vessel_data_send2, ARRAY_SIZE(vessel_data_send2), NULL, 0);
+          err = bt_le_adv_update_data(vessel_data_send2,
+                                      ARRAY_SIZE(vessel_data_send2), NULL, 0);
           break;
         case 2:
-          bt_le_adv_update_data(vessel_data_send3, ARRAY_SIZE(vessel_data_send3), NULL, 0);
+          err = bt_le_adv_update_data(vessel_data_send3,
+                                      ARRAY_SIZE(vessel_data_send3), NULL, 0);
           break;
         case 3:
-          bt_le_adv_update_data(vessel_data_send4, ARRAY_SIZE(vessel_data_send4), NULL, 0);
+          err = bt_le_adv_update_data(vessel_data_send4,
+                                      ARRAY_SIZE(vessel_data_send4), NULL, 0);
           break;
         default:
           break;
-
-
       }
-      //bt_le_adv_update_data(vessel_data_sends[i], ARRAY_SIZE(vessel_data_sends[i]), NULL, 0);
+      if (err) {
+        LOG_ERR("ERROR IN BT UPDATING: %d at index: %d", err, i);
+        return;
+      }
       k_msleep(SEND_PERIOD);
     }
   }
@@ -275,15 +257,22 @@ void bt_task(void* p1, void* p2, void* p3) {
   return;
 }
 
+void print_array(uint8_t arr[]) {
+  LOG_INF("[ ");
+  for (int i = 0; i < 25; i++) {
+    printf("%u ", arr[i]);
+  }
+  LOG_INF("]\n");
+}
 
 void alter_data_task(void* p1, void* p2, void* p3) {
-   struct vessel_data rec_info;
+  struct vessel_data rec_info;
   while (1) {
     if (!k_msgq_get(&bt_msgq, &rec_info, K_NO_WAIT)) {
-        alter_packet(rec_info);
-        gpio_pin_toggle_dt(&led);
-      }
+      alter_packet(rec_info);
+      gpio_pin_toggle_dt(&led);
+      print_array(vessel_data_packet1);
+    }
     k_msleep(50);
   }
-  
 }
--- nrf52840/uart_rec.c
+++ nrf52840/uart_rec.c
@@ -11,10 +11,10 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/types.h>
-
 /* SHELL Command Header Files */
 #include <zephyr/kernel.h>
 #include <zephyr/shell/shell.h>
@@ -26,29 +26,17 @@
 /* JSON Includes */
 #include <zephyr/data/json.h>
 
-// #include "json_nrf_receive.h"
-
 /* JSON Includes */
 #include <zephyr/data/json.h>
 
-#define POLL_INTERVAL_MS 20
-
-// struct k_sem data_sem;
+// setting up pc uart connection
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 
-#define MSG_SIZE 512
-
 struct vessel_data rx_struct;
+LOG_MODULE_DECLARE(AHU, ALL_MODULES);
 
-#define VESSEL_DATA_INIT(_mmsi, _name, _call_sign, _time, _latitude,    \
-                         _longitude, _speed, _heading)                  \
-  {                                                                     \
-    .mmsi = (_mmsi), .vessel_name = (_name), .call_sign = (_call_sign), \
-    .update_time = (_time), .latitude = (_latitude),                    \
-    .longitude = (_longitude), .speed = (_speed), .heading = (_heading) \
-  }
-
+// struct filled with string representation of ais data
 static const struct json_obj_descr JSON_vessel_data_descr[] = {
     JSON_OBJ_DESCR_PRIM(struct vessel_data, mmsi, JSON_TOK_STRING),
     JSON_OBJ_DESCR_PRIM(struct vessel_data, vessel_name, JSON_TOK_STRING),
@@ -58,34 +46,25 @@ static const struct json_obj_descr JSON_vessel_data_descr[] = {
     JSON_OBJ_DESCR_PRIM(struct vessel_data, longitude, JSON_TOK_STRING),
     JSON_OBJ_DESCR_PRIM(struct vessel_data, speed, JSON_TOK_STRING),
     JSON_OBJ_DESCR_PRIM(struct vessel_data, heading, JSON_TOK_STRING),
-    JSON_OBJ_DESCR_PRIM(struct vessel_data, position, JSON_TOK_STRING)
-    // Can't use floats
+    JSON_OBJ_DESCR_PRIM(struct vessel_data, position, JSON_TOK_STRING)};
 
-    /* Maximum value is 32767 for int and 2 147 483 647 for two ints,
-     * Would need mmsi as two ints,
-     * latitude and longitude as 3 ints (1 for upper, 2 for decimal place),
-     and
-     * then speed and heading as 2 ints (1 for upper, 1 for decimal)
-     *
-     * Would almost certainly be better / easier to do strings for all.
-     */
-};
-
+/**
+ * int format_json(char *input_string)
+ * fill json struct with strings from json string
+ * input: json string
+ * output: return value from json_obj_parse
+ */
 int format_json(char *input_string) {
   int err;
-
-  // printk("Input String %s\n", input_string);
-  // printk("Size of %d\n", strlen(input_string));
-
-  // Important to consider command line has max limit of 256 characters.
-  // If there is more than 256 characters there should be concern.
   err =
       json_obj_parse(input_string, strlen(input_string), JSON_vessel_data_descr,
                      ARRAY_SIZE(JSON_vessel_data_descr), &rx_struct);
   return err;
 }
 
+// message position when recieving on uart connection
 int message_pos = 0;
+// buffer to store input json string
 char rx_buf[MSG_SIZE];
 
 // message queue for recieving on second uart line
@@ -93,6 +72,7 @@ K_MSGQ_DEFINE(recieve_msgq, MSG_SIZE, 10, 4);
 
 // message queue for recieving on second uart line
 K_MSGQ_DEFINE(bt_msgq, sizeof(struct vessel_data), 10, 4);
+
 // serial_cb(const struct device *dev, void *user_data)
 //-----------------------------------------------------------------
 // interrupt handler for second uart
@@ -101,24 +81,25 @@ K_MSGQ_DEFINE(bt_msgq, sizeof(struct vessel_data), 10, 4);
 // user_data - data from uart
 void serial_cb(const struct device *dev, void *user_data) {
   uint8_t c;
-
+  // initialising the uart
   if (!uart_irq_update(uart_dev)) {
-    // printk("Error initialising");
+    LOG_ERR("Error initialising");
     return;
   }
 
   if (!uart_irq_rx_ready(uart_dev)) {
-    // printk("Error initialising rx");
+    LOG_ERR("Error initialising rx");
     return;
   }
   while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-    if (c != '$') {
+    if (c != '$') {  // add character to string buffer
       rx_buf[message_pos] = c;
       message_pos++;
     } else {
       // finished
       rx_buf[message_pos] = '\0';
       message_pos = 0;
+      // put on queue to uart thread
       k_msgq_put(&recieve_msgq, &rx_buf, K_NO_WAIT);
     }
   }
@@ -129,7 +110,7 @@ void serial_cb(const struct device *dev, void *user_data) {
 // Uart task waits on message queue for packet to send
 void uart_task(void *p1, void *p2, void *p3) {
   if (!device_is_ready(uart_dev)) {
-    // printk("UART device not found!\n");
+    LOG_ERR("UART device not found!\n");
   }
 
   char buffer[MSG_SIZE];
@@ -139,27 +120,29 @@ void uart_task(void *p1, void *p2, void *p3) {
 
   if (ret < 0) {
     if (ret == -ENOTSUP) {
-      // printk("Interrupt-driven UART API support not enabled\n");
+      LOG_ERR("Interrupt-driven UART API support not enabled\n");
     } else if (ret == -ENOSYS) {
-      // printk("UART device does not support interrupt-driven API\n");
+      LOG_ERR("UART device does not support interrupt-driven API\n");
     } else {
-      // printk("Error setting UART callback: %d\n", ret);
+      LOG_ERR("Error setting UART callback: %d\n", ret);
     }
+    return;
   }
   uart_irq_rx_enable(uart_dev);
   while (1) {
-    // wait indefinitely for message from shell
-    k_msgq_get(&recieve_msgq, &buffer, K_FOREVER);
-    format_json(buffer);
-    // got a struct done
-    //   printk(
-    //       "Recieved a Struct: MMSI: %s, Vessel Name: %s, Call Sign: %s,
-    //       Update " "Time: %s, Latitude: %s, Longitude: %s, Speed: %s,
-    //       Heading: %s, " "Position: %s$", rx_struct.mmsi,
-    //       rx_struct.vessel_name, rx_struct.call_sign, rx_struct.update_time,
-    //       rx_struct.latitude, rx_struct.longitude, rx_struct.speed,
-    //       rx_struct.heading, rx_struct.position);
-    // }
-    k_msgq_put(&bt_msgq, &rx_struct, K_NO_WAIT);
+    // wait indefinitely for message from pc
+    ret = k_msgq_get(&recieve_msgq, &buffer, K_FOREVER);
+    LOG_INF("GETTING HERE!");
+    if (ret) {
+      LOG_WRN("Error reciving information on uart queue: %d", ret);
+    }
+    ret = format_json(buffer);  // format struct
+    if (ret < 0) {
+      LOG_WRN("Error formatting json: %d", ret);
+    }
+    ret = k_msgq_put(&bt_msgq, &rx_struct, K_NO_WAIT);
+    if (ret) {
+      LOG_WRN("Error putting information on uart queue: %d", ret);
+    }
   }
 }
